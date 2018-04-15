6d0632084392e81229903eddfb143de8682a43a3
Nick Athanasiou <nick_10unipi@hotmail.gr>
2018-04-15 20:07:26 +0300

    Removing unused classes

--- WebContent/WEB-INF/jsp/index.jsp
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -20,7 +20,7 @@
 
 
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>TimeSheet Calculator</title>
+<title>HomePage</title>
 </head>
 
 
--- src/main/java/org/nick/service/FormService.java
+++
@@ -1,9 +0,0 @@
-package org.nick.service;
-
-import org.nick.form.TimeSheetForm;
-
-public interface FormService {
-
-	public TimeSheetForm saveFrom(TimeSheetForm timesheetForm);
-	public TimeSheetForm updateForm(TimeSheetForm timesheetForm);
-}
